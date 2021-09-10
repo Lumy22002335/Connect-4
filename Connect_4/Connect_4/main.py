8fa15c025a14d01a6bceaf18a9071f88264ed5f1
Lumy <blndogsun@hotmail.com>
2021-09-10 17:54:03 +0100

    Open basic window and read events

--- Connect_4/Connect_4/main.py
+++ Connect_4/Connect_4/main.py
@@ -1,4 +1,14 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((860, 480))
+
 def main():
-    print("Hello World!")
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 main()
