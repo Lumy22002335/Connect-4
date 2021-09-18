075157f7e3a6b669612cf26f5841782e8cbee9d0
Lumy22002335 <blndogsun@hotmail.com>
2021-09-18 02:54:48 +0100

    Finish menu and credits

--- Connect_4/Connect_4/menu.py
+++ Connect_4/Connect_4/menu.py
@@ -9,21 +9,27 @@ class Menu:
 
         self.red_circle = pygame.image.load("Red_Chip_Big.png")
         self.yellow_circle = pygame.image.load("Yellow_Chip_Big.png")
-
+        self.credits_image = pygame.image.load("Credits.png")
         self.title = pygame.image.load("Title.png")
 
         self.buttons = []
         self.buttons.append(Button((322, 230), (215, 67), pygame.image.load("Play_Default.png"), pygame.image.load("Play_Highlight.png"), lambda: self.play()))
         self.buttons.append(Button((322, 290), (215, 67), pygame.image.load("Quit_Default.png"), pygame.image.load("Quit_Highlight.png"), lambda: self.exit()))
         self.buttons.append(Button((322, 350), (215, 67), pygame.image.load("Credits_Default.png"), pygame.image.load("Credits_Highlight.png"), lambda: self.display_credits()))
+        self.buttons.append(Button((0, 380), (215, 67), pygame.image.load("Back_Default.png"), pygame.image.load("Back_Highlight.png"), lambda: self.back()))
     
     def render(self, screen):
         screen.fill((40, 20, 0))
-        screen.blit(self.title, (228, 0))
-        screen.blit(self.red_circle, (20, 125))
-        screen.blit(self.yellow_circle, (584, 125))
-        for b in self.buttons:
-            b.draw(screen)
+        if self.credits:
+            screen.blit(self.credits_image, (167, 10))
+            self.buttons[3].draw(screen)
+        else:
+            screen.blit(self.title, (228, 0))
+            screen.blit(self.red_circle, (20, 125))
+            screen.blit(self.yellow_circle, (584, 125))
+            self.buttons[0].draw(screen)
+            self.buttons[1].draw(screen)
+            self.buttons[2].draw(screen)
 
     def event_handler(self, event):
         if event.type == pygame.MOUSEBUTTONUP:
@@ -35,12 +41,17 @@ class Menu:
                 b.event_handler(event)
 
     def play(self):
-        self.start = True
+        if self.credits == False:
+            self.start = True
 
     def exit(self):
-        self.quit = True
+        if self.credits == False:
+            self.quit = True
 
     def display_credits(self):
         self.credits = True
+
+    def back(self):
+        self.credits = False
 
 
