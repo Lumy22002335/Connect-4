84873b844c3ae82c595cc4a6af31e23af39588a1
Lumy22002335 <blndogsun@hotmail.com>
2021-09-19 17:36:47 +0100

    Can press play and back out

--- Connect_4/Connect_4/game.py
+++ Connect_4/Connect_4/game.py
@@ -5,5 +5,22 @@ class Game:
 	def __init__(self):
 		self.quit = False
 
+		self.red_chip = pygame.image.load("Red_Chip.png")
+		self.yellow_chip = pygame.image.load("Yellow_Chip.png")
+
+		self.back_button = Button((-25, 405), (215, 67), pygame.image.load("Back_Default.png"), pygame.image.load("Back_Highlight.png"), lambda: self.back())
+
 	def render(self, screen):
-		screen.fill((20, 40, 0))
+		screen.fill((40, 20, 0))
+		self.back_button.draw(screen)
+
+	def event_handler(self, event):
+		if event.type == pygame.MOUSEBUTTONUP:
+			if self.back_button.rect.collidepoint(event.pos):
+				self.back_button.on_click()
+
+		else:
+			self.back_button.event_handler(event)
+
+	def back(self):
+		self.quit = True
